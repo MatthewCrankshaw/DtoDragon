6d4ba2b7786dcbf01746b15b835fa5a6bb288d46
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2022-03-19 20:40:56 +1300

    Added doc blocks to the DtoReflector class

--- src/utilities/DtoReflector.php
+++ src/utilities/DtoReflector.php
@@ -8,23 +8,45 @@ use DtoDragon\interfaces\ReflectorInterface;
 use ReflectionClass;
 use ReflectionProperty;
 
+/**
+ * Class that reports information about the data transfer object class and properties
+ *
+ * @package DtoDragon\utilities
+ *
+ * @author Matthew Crankshaw
+ */
 class DtoReflector implements ReflectorInterface
 {
+    /**
+     * The data transfer object being reported and acted upon
+     *
+     * @var DataTransferObject
+     */
     private DataTransferObject $dto;
 
+    /**
+     * The reflection of the data transfer object
+     *
+     * @var ReflectionClass
+     */
     private ReflectionClass $dtoReflection;
 
+    /**
+     * Construct the data transfer object reflector
+     *
+     * @param DataTransferObject $dto
+     */
     public function __construct(DataTransferObject $dto)
     {
         $this->dto = $dto;
-        $this->dtoReflection = $this->createClassReflection();
+        $this->dtoReflection = new ReflectionClass($dto);
     }
 
-    private function createClassReflection(): ReflectionClass
-    {
-        return new ReflectionClass($this->dto);
-    }
-
+    /**
+     * Get the data transfer object that this reflector is reporting on
+     *
+     * @return DataTransferObject
+     */
     public function getDto(): DataTransferObject
     {
         return $this->dto;
@@ -56,18 +78,42 @@ class DtoReflector implements ReflectorInterface
         $property->setValue($this->dto, $value);
     }
 
+    /**
+     * Returns true if the reflection property provided is a DTO
+     * Otherwise false
+     *
+     * @param ReflectionProperty $property
+     *
+     * @return bool
+     */
     public function propertyIsDto(ReflectionProperty $property): bool
     {
         $type = $property->getType()->getName();
         return is_subclass_of($type, DataTransferObject::class);
     }
 
+    /**
+     * Returns true if the reflection property provided is a collection
+     * Otherwise false
+     *
+     * @param ReflectionProperty $property
+     *
+     * @return bool
+     */
     public function propertyIsCollection(ReflectionProperty $property): bool
     {
         $type = $property->getType()->getName();
         return is_subclass_of($type, DataTransferObjectCollection::class);
     }
 
+    /**
+     * Returns true if the reflection property provided is an array
+     * Otherwise false
+     *
+     * @param ReflectionProperty $property
+     *
+     * @return bool
+     */
     public function propertyIsArray(ReflectionProperty $property): bool
     {
         $type = $property->getType()->getName();
