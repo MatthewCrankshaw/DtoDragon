ed844dee19ad3960ee1e6d85dd0e43f0744d65b1
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2022-03-19 19:48:17 +1300

    Added doc blocks to the DtoReflectorFactory class

--- src/utilities/DtoReflectorFactory.php
+++ src/utilities/DtoReflectorFactory.php
@@ -5,16 +5,35 @@ namespace DtoDragon\utilities;
 use DtoDragon\DataTransferObject;
 use DtoDragon\interfaces\ReflectorInterface;
 
+/**
+ * Factory for creating DTO Reflectors for a particular DTO
+ *
+ * @package DtoDragon\utilities
+ *
+ * @author Matthew Crankshaw
+ */
 class DtoReflectorFactory
 {
+    /**
+     * The data transfer object we would like the factory to create a reflector for
+     *
+     * @var DataTransferObject
+     */
     public DataTransferObject $dto;
 
+    /**
+     * Construct the data transfer object reflector factory
+     *
+     * @param DataTransferObject $dto
+     */
     public function __construct(DataTransferObject $dto)
     {
         $this->dto = $dto;
     }
 
     /**
+     * Create the data transfer reflector for the DTO
+     *
      * @return DtoReflector
      */
     public function create(): ReflectorInterface
