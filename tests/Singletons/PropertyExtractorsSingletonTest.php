7be13652405d4412bb6d936f1a874e118e1f6d50
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2022-04-16 11:09:48 +1200

    feat: added full unit test coverage to the PropertyExtractorSingleton class

--- tests/Singletons/PropertyExtractorsSingletonTest.php
+++ tests/Singletons/PropertyExtractorsSingletonTest.php
@@ -6,6 +6,10 @@ use DtoDragon\Singletons\PropertyExtractorsSingleton;
 use DtoDragon\Test\PropertyExtractor\DatePropertyExtractor;
 use DtoDragon\Test\DtoDragonTestCase;
 use DtoDragon\Test\TestDtos\Date;
+use DtoDragon\Test\TestDtos\ServiceCollection;
+use DtoDragon\Test\TestDtos\ServiceDto;
+use DtoDragon\Utilities\Extractor\PropertyExtractors\CollectionPropertyExtractor;
+use DtoDragon\Utilities\Extractor\PropertyExtractors\DtoPropertyExtractor;
 
 /**
  * @covers \DtoDragon\Singletons\PropertyExtractorsSingleton
@@ -13,36 +17,82 @@ use DtoDragon\Test\TestDtos\Date;
  */
 class PropertyExtractorsSingletonTest extends DtoDragonTestCase
 {
-    private PropertyExtractorsSingleton $propertyExtractors;
+    private PropertyExtractorsSingleton $singleton;
 
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $this->propertyExtractors = PropertyExtractorsSingleton::getInstance();
+        $this->singleton = PropertyExtractorsSingleton::getInstance();
+    }
+
+    public function testClear(): void
+    {
+        $this->singleton->register(new DatePropertyExtractor());
+
+        $propertyExtractors = $this->getProtectedProperty($this->singleton, 'propertyExtractors');
+        $this->assertNotEmpty($propertyExtractors);
+
+        $this->singleton->clear();
+        $propertyExtractors = $this->getProtectedProperty($this->singleton, 'propertyExtractors');
+        $this->assertEmpty($propertyExtractors);
     }
 
     public function testRegister(): void
     {
-        $this->propertyExtractors->register(new DatePropertyExtractor());
-        $propertyExtractors = $this->getProtectedProperty($this->propertyExtractors, 'propertyExtractors');
+        $this->singleton->register(new DatePropertyExtractor());
+        $propertyExtractors = $this->getProtectedProperty($this->singleton, 'propertyExtractors');
 
-        $this->assertCount(7, $propertyExtractors);
+        $this->assertCount(1, $propertyExtractors);
     }
 
     public function testGetPropertyExtractor(): void
     {
-        $propertyExtractor = $this->propertyExtractors->getPropertyExtractor(Date::class);
+        $propertyExtractor = $this->singleton->getPropertyExtractor(Date::class);
 
         $this->assertInstanceOf(DatePropertyExtractor::class, $propertyExtractor);
     }
 
     public function testHasPropertyExtractor(): void
     {
-        $this->assertTrue($this->propertyExtractors->hasPropertyExtractor(Date::class));
+        $this->assertTrue($this->singleton->hasPropertyExtractor(Date::class));
+    }
+
+    public function testHasDtoPropertyExtractor(): void
+    {
+        $this->singleton->register(new DtoPropertyExtractor());
+        $this->assertTrue($this->singleton->hasPropertyExtractor(ServiceDto::class));
+    }
+
+    public function testHasCollectionPropertyExtractor(): void
+    {
+        $this->singleton->register(new CollectionPropertyExtractor());
+        $this->assertTrue($this->singleton->hasPropertyExtractor(ServiceCollection::class));
     }
 
     public function testDoesNotHavePropertyExtractor(): void
     {
-        $this->assertFalse($this->propertyExtractors->hasPropertyExtractor(PropertyExtractorsSingletonTest::class));
+        $this->assertFalse($this->singleton->hasPropertyExtractor(PropertyExtractorsSingletonTest::class));
+    }
+
+    public function testGetDtoPropertyExtractor(): void
+    {
+        $this->singleton->register(new DtoPropertyExtractor());
+
+        $extractor = $this->singleton->getPropertyExtractor(ServiceDto::class);
+        $this->assertInstanceOf(DtoPropertyExtractor::class, $extractor);
+    }
+
+    public function testGetCollectionPropertyController(): void
+    {
+        $this->singleton->register(new CollectionPropertyExtractor());
+
+        $extractor = $this->singleton->getPropertyExtractor(ServiceCollection::class);
+        $this->assertInstanceOf(CollectionPropertyExtractor::class, $extractor);
+    }
+
+    public function testCannotGetPropertyExtractor(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->singleton->getPropertyExtractor(PropertyExtractorsSingletonTest::class);
     }
 }
